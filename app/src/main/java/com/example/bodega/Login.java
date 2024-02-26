06e8e0b26298ecba4834bbb8bf5f8efc6feb176a
JoseDavid210803 <jose.castellanos4652@alumnos.udg.mx>
2024-02-26 08:41:46 -0600

    Funcionalidad boton Iniciar sesion

--- app/src/main/java/com/example/bodega/Login.java
+++ app/src/main/java/com/example/bodega/Login.java
@@ -1,4 +1,17 @@
 package com.example.bodega;
 
-public class Login {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
 }
---
+++ app/src/main/java/com/example/bodega/Registro.java
@@ -0,0 +1,4 @@
+package com.example.bodega;
+
+public class Registro {
+}
