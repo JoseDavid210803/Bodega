c0e33f73ec2304b23f8482457e469b104ab4e318
JoseDavid210803 <jose.castellanos4652@alumnos.udg.mx>
2024-03-01 17:59:35 -0600

    Registro

--- app/src/main/java/com/example/bodega/Registro.java
+++ app/src/main/java/com/example/bodega/Registro.java
@@ -1,4 +1,21 @@
 package com.example.bodega;
 
-public class Registro {
+import static java.lang.Thread.sleep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+
+import com.google.android.material.slider.RangeSlider;
+
+public class Registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+
+    }
 }
