a842daf31a72d53f93800403654f2f39f09b85fc
Marcos Castellanos Villaseñor <marcos.castellanos7815@alumnos.udg.mx>
2024-04-25 12:21:02 -0600

    boceto main page

---
+++ app/src/main/java/com/example/bodega/HomePage.java
@@ -0,0 +1,18 @@
+package com.example.bodega;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+
+    }
+
+    }
+
